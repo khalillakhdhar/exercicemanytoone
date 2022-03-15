c2e5acd677cfaea1ef7f33399671f9f57d09315a
khalil <khalillakhdharatc@gmail.com>
2022-03-15 09:31:58 +0100

    Initial commit

---
+++ src/main/java/com/duranco/ExercicemanytooneApplication.java
@@ -0,0 +1,13 @@
+package com.duranco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicemanytooneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicemanytooneApplication.class, args);
+	}
+
+}
