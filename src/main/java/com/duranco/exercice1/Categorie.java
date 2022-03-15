e4804765e2284b7d00ce3a0dce47790cc0e9b054
khalil <khalillakhdharatc@gmail.com>
2022-03-15 09:38:23 +0100

    ones

---
+++ src/main/java/com/duranco/exercice1/Categorie.java
@@ -0,0 +1,33 @@
+package com.duranco.exercice1;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categorie {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+@Column(unique = true)
+private String titre;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getTitre() {
+	return titre;
+}
+public void setTitre(String titre) {
+	this.titre = titre;
+}
+
+
+
+
+
+}
---
+++ src/main/java/com/duranco/exercice1/User.java
@@ -0,0 +1,67 @@
+package com.duranco.exercice1;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+@Column(unique = true)
+private String email;
+private String mdp;
+private String nom;
+private String adresse;
+private String grade;
+
+private String telephone;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getMdp() {
+	return mdp;
+}
+public void setMdp(String mdp) {
+	this.mdp = mdp;
+}
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getAdresse() {
+	return adresse;
+}
+public void setAdresse(String adresse) {
+	this.adresse = adresse;
+}
+public String getGrade() {
+	return grade;
+}
+public void setGrade(String grade) {
+	this.grade = grade;
+}
+public String getTelephone() {
+	return telephone;
+}
+public void setTelephone(String telephone) {
+	this.telephone = telephone;
+}
+
+
+}
